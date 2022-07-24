dcf21ec91edb9666c400b2a5d459c7cc963d099e
indhu <indhusiramdasu@gmail.com>
2022-07-24 07:28:10 +0530

    Calculator is added

---
+++ src/javaprogram/Calculator.java
@@ -0,0 +1,25 @@
+package javaprogram;
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+
+        String operation = "Sub";
+
+        switch (operation) {
+            case "Add":
+                System.out.println(num1 + num2);
+                break;
+            case "Mul":
+                System.out.println(num1 * num2);
+                break;
+            case "Sub":
+                System.out.println(num1 - num2);
+                break;
+            case "Div":
+                System.out.println(num1 / num2);
+                break;
+            default:
+                System.out.println("Enter valid operation");
+        }
+    }
+}
